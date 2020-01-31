1d19e992809792ecf7ce4add6d0cbe58f904982c
zubairaltaf <9044452+zubairaltaf@users.noreply.github.com>
2020-01-31 13:46:28 +1100

    change the content

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Zubair! You have done it again"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
